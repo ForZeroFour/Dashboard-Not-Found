7a337898f21dbea3b1af2f02a65e272b0e93ee6a
StEugen <85479518+StEugen@users.noreply.github.com>
2023-09-27 00:16:24 +0400

    some stuff, preparing for work

---
+++ backend/dashboard/templates/index.js
@@ -0,0 +1,13 @@
+<!-- your_template.html -->
+{% load vite_tags %}
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Other head content -->
+    {% vite_script "my-vue-app" %}
+</head>
+<body>
+    <!-- Your content -->
+</body>
+</html>
+
