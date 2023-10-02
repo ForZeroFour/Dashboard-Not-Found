aa159632e6f95b782a8745dd8b964a272429b364
gygever <toneev.toly@yandex.ru>
2023-10-02 20:04:12 +0400

    model Item
    надо посмотреть информацию про папку, для сохранения изображений

--- backend/dashboard/models.py
+++ backend/dashboard/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Item(models.Model):
+    icons = models.ImageField(required=False)
+    name = models.CharField(max_length=256)
+    url = models.URLField(max_length=512)
